29cea2b8eba540fd980db74aa61b58d9d0b354f3
MatthewHtang <matthewhalingthang28@gmail.com>
2026-09-08 10:27:25 -0500

    Enhance main menu display and add new header/source files for resource management

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,24 @@ int main() {
     //do while loop to run the menu first and check the condition
     int input = 0;
     do {
-        cout << "\n===== Campus Resource Reservation System =====\n" << endl;
-        cout << "1. View Resources" << endl;
-        cout << "2. Create Reservation" << endl;
-        cout << "3. Cancel Reservation" << endl;
-        cout << "4. View Waiting Lists" << endl;
-        cout << "5. Undo Cancellation" << endl;
-        cout << "6. Search Reservations" << endl;
-        cout << "7. Sort Resources" << endl;
-        cout << "8. Generate Report" << endl;
-        cout << "9. Exit\n" << endl;
-        cout << "Enter Choice: ";
-
+        cout << "\n";
+        cout << "===================================================";
+        cout << "        Campus Resource Reservation System\n";
+        cout << "===================================================";
+        cout << "   1. View Resources\n";
+        cout << "   2. Create Reservation\n";
+        cout << "   3. Cancel Reservation\n";
+        cout << "   4. View Waiting Lists\n";
+        cout << "   5. Undo Cancellation\n";
+        cout << "   6. Search Reservations\n";
+        cout << "   7. Sort Resources\n";
+        cout << "   8. Generate Report\n";
+        cout << "   9. Exit\n";
+        cout << "---------------------------------------------------\n";
+        cout << "   Enter Choice: ";
+        
         cin >> input;
+        
     } while (input != 9);
     
     return 0;
