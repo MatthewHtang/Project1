b8f6b06046a7edd9c42bd004c0d58e5121155da0
MatthewHtang <matthewhalingthang28@gmail.com>
2026-09-08 10:30:01 -0500

    Fix Bugs for main menu display

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
@@ -8,23 +9,23 @@ int main() {
     int input = 0;
     do {
         cout << "\n";
-        cout << "===================================================";
+        cout << "===================================================\n";
         cout << "        Campus Resource Reservation System\n";
-        cout << "===================================================";
-        cout << "   1. View Resources\n";
-        cout << "   2. Create Reservation\n";
-        cout << "   3. Cancel Reservation\n";
-        cout << "   4. View Waiting Lists\n";
-        cout << "   5. Undo Cancellation\n";
-        cout << "   6. Search Reservations\n";
-        cout << "   7. Sort Resources\n";
-        cout << "   8. Generate Report\n";
-        cout << "   9. Exit\n";
+        cout << "===================================================\n";
+        cout << "       1. View Resources\n";
+        cout << "       2. Create Reservation\n";
+        cout << "       3. Cancel Reservation\n";
+        cout << "       4. View Waiting Lists\n";
+        cout << "       5. Undo Cancellation\n";
+        cout << "       6. Search Reservations\n";
+        cout << "       7. Sort Resources\n";
+        cout << "       8. Generate Report\n";
+        cout << "       9. Exit\n";
         cout << "---------------------------------------------------\n";
-        cout << "   Enter Choice: ";
+        cout << "       Enter Choice: ";
         
         cin >> input;
-        
+
     } while (input != 9);
     
     return 0;
