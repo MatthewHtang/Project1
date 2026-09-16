95e32d84733e075d6410fb2b49537f22e24008f0
Sankalp Gupta <sankalpgupta@Mac-2027.lan>
2026-09-15 20:39:47 -0500

    Added ResourceManager class and comments

---
+++ include/ResourceManager.h
@@ -0,0 +1,30 @@
+#ifndef RESOURCEMANAGER_H  
+#define RESOURCEMANAGER_H   
+
+#include "Resource.h"   // uses the Resource class
+#include <vector>       // used to store multiple resources
+#include <string>       // used for file names and text
+using namespace std;
+
+class ResourceManager { // manages a collection of resources
+
+private:
+
+    vector<Resource> resources;     // Vector to store all resources in memory
+
+public:
+    bool loadFromFile(string filename); // loads reosurces from the file into the resources vector
+
+    void displayAll();                  // displays all resources
+    void displayAvailable();            // displays resources that are available (not in use)
+
+    Resource* findById(int resourceId); // finds a reosurce by its ID
+    bool exists(int resourceId);        // checks if the resource ID exists in the resources vector
+    bool setInUse(int resourceId, bool inUse);     // changes the resouce's status
+
+    int getCount();                     // returns the number of resources in the resources vector
+    bool isEmpty();                     // checks if the resources vector is empty
+
+};
+
+#endif 
